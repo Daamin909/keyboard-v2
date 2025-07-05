2db7d62fb0f3794bcac5cdbdafc6ce52a5bfbfcd
Daamin909 <ashai.daamin@gmail.com>
2025-07-05 23:05:45 +0530

    added backtick

--- firmware/code.py
+++ firmware/code.py
@@ -95,7 +95,7 @@ keyboard.keymap = [
     ],
     # Layer 1 - Alternate Layer
     [
-        KC.TILDE,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.MINS, KC.EQL,  KC.BSPC,
+        KC.GRAVE,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.MINS, KC.EQL,  KC.BSPC,
         KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.LBRC, KC.RBRC, KC.BSLS,
         KC.CAPS,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOT, KC.ENT,  KC.MUTE,
         KC.LSFT,   KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.UP,   KC.RSFT, KC.NO, KC.MEDIA_PLAY_PAUSE,
